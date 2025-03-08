7e1cad6eab4e22b11cce997507be1d98a46076ba
Zain Ali <zainidrees3830@gmail.com>
2025-03-08 07:01:00 -0800

    commit

--- index.js
+++
@@ -1,7 +0,0 @@
-let addToCart=document.createElement('button');
-// addToCart.classList.add('addToCart')
-addToCart.innerHTML="Cartu"
-addToCart.addEventListener('click',()=>{
-    // window.location='cart.html'
-   
-})
